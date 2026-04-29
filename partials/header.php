eee8dca2f00ed7a82a36b809fd7a73392be247a4
GutsGusta <gustavonovaes623@gmail.com>
2026-04-29 16:43:36 -0300

    Começo do cadastro

---
+++ partials/header.php
@@ -0,0 +1,9 @@
+    <header>
+        <h1 class="titulo">Bem-vindo à sua Playlist de Músicas</h1>
+    </header>
+
+    <div class="botoes-nav">
+        <a href="cadastro.html" class="botao-nav">Adicionar Música</a>
+        <a href="#" class="botao-nav">Editar Música</a>
+        <a href="#" class="botao-nav">Excluir Música</a>
+    </div>
